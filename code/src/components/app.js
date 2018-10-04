e6fc108d917547d5ad8075e7a7d9758f3e01aedd
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-10-04 14:48:13 +0200

    added if statement

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -22,22 +22,26 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="imageHeadline"><img src="https://sverigesradio.se/kvinnoregistret/assets/images/sr-logo-white.png" alt="Sveriges Radio" /></div>
-        <div className="stationContainer">
-          {this.state.radioItems.map((channels) => {
-            return <Station
-              name={channels.name}
-              image={channels.image}
-              audio={channels.liveaudio.url}
-              color={channels.color}
-              tagline={channels.tagline}
-              channeltype={channels.channeltype} />
-          })}
+    if (this.state.radioItems.length > 0) {
+      return (
+        <div>
+          <div className="imageHeadline"><img src="https://sverigesradio.se/kvinnoregistret/assets/images/sr-logo-white.png" alt="Sveriges Radio" /></div>
+          <div className="stationContainer">
+            {this.state.radioItems.map((channels) => {
+              return <Station
+                name={channels.name}
+                image={channels.image}
+                audio={channels.liveaudio.url}
+                color={channels.color}
+                tagline={channels.tagline}
+                channeltype={channels.channeltype} />
+            })}
+          </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return <div> Loading..</div>
+    }
   }
 
 }
